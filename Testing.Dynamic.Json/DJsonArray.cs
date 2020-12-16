b3f19a2de52934739d5e547c2942ae69b2ef7e69
Tho Ho <code@tho.email>
2020-12-16 18:41:05 +0800

    Remove unused dynamic TryGetIndex

--- Testing.Dynamic.Json/DJsonArray.cs
+++ Testing.Dynamic.Json/DJsonArray.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Dynamic;
 using System.Linq;
 using System.Text.Json.Serialization;
 
@@ -15,12 +14,6 @@ namespace Testing.Dynamic
         public override int CountMembers => _list.Count;
 
         public override List<object> InnerList => _list;
-
-        public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object result)
-        {
-            result = indexes.Length == 1 && indexes[0] is int index && index >= 0 && index < _list.Count ? _list[index] : null;
-            return true;
-        }
 
         public override object this[int index] => index >= 0 && index < _list.Count ? _list[index] : null;
     }
