fc958b6d21f6f3eb478322d3cc495318650b853a
Tho Ho <code@tho.email>
2021-09-17 12:27:24 +0800

    Support multi TargetFrameworks: net6.0, net5.0, netstandard2.0; update package tags

---
+++ samples/ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NetLah.Diagnostics;
+using Testing.Dynamic;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var appInfo = ApplicationInfo.Initialize(null);
+            Console.WriteLine($"AppTitle: {appInfo.Title}");
+            Console.WriteLine($"Version:{appInfo.InformationalVersion} BuildTime:{appInfo.BuildTimestampLocal}; Framework:{appInfo.FrameworkName}");
+
+            var asmAbstracts = new AssemblyInfo(typeof(AssemblyInfo).Assembly);
+            Console.WriteLine($"AssemblyTitle: {asmAbstracts.Title}");
+            Console.WriteLine($"Version:{asmAbstracts.InformationalVersion} BuildTime:{asmAbstracts.BuildTimestampLocal}; Framework:{asmAbstracts.FrameworkName}");
+
+            var asmConfig = new AssemblyInfo(typeof(DJson).Assembly);
+            Console.WriteLine($"AssemblyTitle: {asmConfig.Title}");
+            Console.WriteLine($"Version:{asmConfig.InformationalVersion} BuildTime:{asmConfig.BuildTimestampLocal}; Framework:{asmConfig.FrameworkName}");
+        }
+    }
+}
